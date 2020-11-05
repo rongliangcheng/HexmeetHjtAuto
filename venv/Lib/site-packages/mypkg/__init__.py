82b10a836588758bcf93e3d2bef68b601654464a
rongliangcheng <cheng_rongliang@163.com>
2020-11-05 17:08:45 +0800

    nenv

---
+++ venv/Lib/site-packages/mypkg/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is the documentation for the package.
+"""
+
+__version__ = '1.0.0'
---
+++ venv/Lib/site-packages/mypkg/main.py
@@ -0,0 +1,7 @@
+""" This is a basic docstring. """
+
+import os
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
