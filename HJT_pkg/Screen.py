10a72bcbabb65162405fbea405a700005e49eefb
rongliang <cheng_rongliang@163.com>
2020-11-11 09:12:59 +0800

    add waiting timer

--- HJT_pkg/Screen.py
+++ HJT_pkg/Screen.py
@@ -3,19 +3,19 @@ from PIL import ImageGrab
 import allure
 
 
-
 class CaptureScreen:
 
     def capture_screen(self, pic_name):
         im = ImageGrab.grab(bbox=(645, 316, 1918, 1072))
-        im.save(pic_name+".png", "jpeg")
+        im.save(pic_name, "jpeg")
 
     def attach_pic(self, pic_name, description):
         with open(pic_name, mode='rb') as f:
             file = f.read()
             allure.attach(file, description, allure.attachment_type.PNG)
 
+
 if __name__ == '__main__':
     sleep(3)
     capture_app = CaptureScreen()
-    capture_app.capture_screen("test")
+    capture_app.capture_screen("test.png")
