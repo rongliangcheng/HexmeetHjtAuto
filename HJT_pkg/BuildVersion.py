ffeaacf3e47e3fbeaabac9c523e6b0b2d3300594
rongliang <cheng_rongliang@163.com>
2020-11-16 10:08:55 +0800

    clear reserved meeting before start

--- HJT_pkg/BuildVersion.py
+++ HJT_pkg/BuildVersion.py
@@ -1,30 +1,43 @@
 from time import sleep
-import urllib
+import urllib.parse
+from urllib.request import *
 from http import cookiejar as cookielib
 
 import requests
+from requests.auth import AuthBase
+from requests.auth import HTTPBasicAuth
+
 
 
 class BuildVersion:
     def __init__(self):
         self.buildUrl = "http://172.27.0.12:8080/view/HJT%20SVC%20EVSDK%20APP/job/swep-evsdk-win-svc-qt-1.4.0/"
-        self.session = requests.session()
-        self.session.cookies = cookielib.LWPCookieJar(filename="huihuCookies.txt")
+        # self.session = requests.session()
+        # self.session.cookies = cookielib.LWPCookieJar(filename="huihuCookies.txt")
+
+    # def alogin(self):
+    #     loginUrl = "http://172.27.0.12:8080/login?from=%2Fview%2FHJT%2520SVC%2520EVSDK%2520APP%2Fjob%2Fswep-evsdk-win-svc-qt-1.4.0%2F"
+    #     userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+    #     header = {
+    #         "Referer": loginUrl,
+    #         'User-Agent': userAgent,
+    #     }
+    #     post_data = {
+    #         "username": "chengrl",
+    #         "password": "123456",
+    #     }
+    #
+    #     res = self.session.post(loginUrl, data=post_data, headers=header)
+    #     print(res.text)
 
     def login(self):
-        loginUrl = "http://172.27.0.12:8080/login?from=%2Fview%2FHJT%2520SVC%2520EVSDK%2520APP%2Fjob%2Fswep-evsdk-win-svc-qt-1.4.0%2F"
-        userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
-        header = {
-            "Referer": loginUrl,
-            'User-Agent': userAgent,
-        }
-        post_data = {
-            "username": "chengrl",
-            "password": "123456",
-        }
+        username = "chengrl"
+        password = "123456"
 
-        res = self.session.post(loginUrl, data=post_data, headers=header)
-        print(res.text)
+        auth = HTTPBasicAuth(username, password)
+        loginUrl = "http://172.27.0.12:8080/login?from=%2Fview%2FHJT%2520SVC%2520EVSDK%2520APP%2Fjob%2Fswep-evsdk-win-svc-qt-1.4.0%2F"
+        response = requests.post(loginUrl, auth=auth)
+        print(response.text)
 
     def getBuildVersion(self):
         try:
@@ -37,4 +50,3 @@ class BuildVersion:
 
 if __name__ == '__main__':
     BuildVersion().login()
-    BuildVersion().getBuildVersion()
--- test/main.py
+++ test/main.py
@@ -7,5 +7,5 @@ sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == '__main__':
     pytest.main(["-v", "-s", "./appsetting/test_appsetting.py"])
-    # pytest.main(["-v", "-s", "./login/test_login.py",  "./contact/test_contact.py", "./appsetting/test_appsetting.py",
+    # pytest.main(["-v", "-s", "./alogin/test_login.py",  "./contact/test_contact.py", "./appsetting/test_appsetting.py",
     #              "./operateinameetingtest/test_operatemeeting.py", "./reservemeetingtest/test_reservemeeting.py", "./unregistercall/test_unregistercall.py", "--alluredir=reports"])
